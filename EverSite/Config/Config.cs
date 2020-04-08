4836ae3e35767e7ff2f579d81af8acccf3c6a9e4
Mike White <mike.wildcats@gmail.com>
2020-04-07 22:16:45 -0400

    Initial scaffolding

---
+++ EverSite/Config/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EverSite.Config
+{
+    public class Config
+    {
+        public static string Connection = "Use this to connect";
+    }
+}
---
+++ EverSite/Data/MemberContext.cs
@@ -0,0 +1,18 @@
+﻿using EverSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EverSite.Data
+{
+    public class MemberContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(Config.Config.Connection);
+        }
+    }
+}
